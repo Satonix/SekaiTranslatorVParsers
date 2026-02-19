7a6dd2db775c36d8a1ba19ebd81cf280daeb874d
Lucas Araçá <121361767+Satonix@users.noreply.github.com>
2026-02-18 23:18:18 -0300

    Update __init__.py

--- src/sekai_parsers/engines/kirikiri/__init__.py
+++ src/sekai_parsers/engines/kirikiri/__init__.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from ...engine_registry import register_engine
 from .ks_parser import KiriKiriKsParser
 
+
 def _factory():
     return KiriKiriKsParser()
+
 
 register_engine("kirikiri.ks", _factory)
