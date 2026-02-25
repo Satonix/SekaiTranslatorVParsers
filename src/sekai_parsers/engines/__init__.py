6b02503d1c1825fb72d6d0d2b130706dab589fba
Satonix <satonix007@gmail.com>
2026-02-25 16:51:16 -0300

    Bugs...

--- src/sekai_parsers/engines/__init__.py
+++ src/sekai_parsers/engines/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 # pacote marcador; discovery é feito por pkgutil no sekai_parsers/__init__.py
-=======
-# pacote marcador; discovery é feito por pkgutil no sekai_parsers/__init__.py
->>>>>>> 824d17b2d4c0216bd447d690127f0ff6d4259d4a
