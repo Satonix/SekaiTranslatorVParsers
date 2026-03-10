2e5ab3757db80d8636417825d9946e3951ba1b19
Satonix <satonix007@gmail.com>
2026-03-10 16:11:58 -0300

    Engine YU-RIS

--- src/sekai_parsers/engines/yuris/profiles/euphoria.py
+++ src/sekai_parsers/engines/yuris/profiles/euphoria.py
@@ -7,6 +7,8 @@ EUPHORIA_PROFILE = YuRisProfile(
     id="euphoria",
     ignore_exact=(
         "NEM",
+        "white",
+        "black",
     ),
     ignore_prefixes=(
         "\\\\GO(",
@@ -15,6 +17,7 @@ EUPHORIA_PROFILE = YuRisProfile(
         "bg_",
         "bgm",
         "st_",
+        "ysr",
     ),
     ignore_regexes=(
         r"^\\\\[A-Za-z_]+\s*\(.*\)$",
@@ -23,6 +26,8 @@ EUPHORIA_PROFILE = YuRisProfile(
         r"^bg_[A-Za-z0-9_]+$",
         r"^bgm[0-9A-Za-z_]+$",
         r"^st_[A-Za-z0-9_]+$",
+        r"^ysr[0-9A-Za-z_]+$",
+        r"^[A-Z]{2,6}_M[0-9]{2}_[0-9]{4}$",
         r"^[A-Z0-9_]{2,12}$",
     ),
     speaker_line_regexes=(
